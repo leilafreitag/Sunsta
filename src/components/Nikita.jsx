e13393dd692bb02e6e5c5f24a03f6bfd0cda4bd9
UpstairsForest <nikita.batalov@uzh.ch>
2022-11-13 16:56:15 +0100

    Larger button

--- src/components/Nikita.jsx
+++ src/components/Nikita.jsx
@@ -1,8 +1,8 @@
 export function Nikita() {
     return (
         <div>
-            <h1>This is Nikita</h1>
-            <div>penis</div>
+            <h1>This is Nikita aaaaaaaaaaaaaaa</h1>
+            <button style={{width: "100%", height: "100px"}}>penis</button>
         </div>
     );
 }
